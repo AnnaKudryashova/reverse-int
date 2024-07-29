efeb16fce6fae548386911535a72776b32c0181a
Anna Kudriashova <amarenoru@gmail.com>
2024-07-29 14:44:12 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function reverse (n) {
-  
+    const str = n.toString();
+    const reversedStr = str.split('').reverse().join('');
+    const result = parseInt(reversedStr, 10);
+    return result;
 }
